70eb44665590a79b5734b207c1aa81b3c60417b4
Your Name <you@example.com>
2024-08-06 18:59:21 +0530

    Add 6-8

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,38 @@ const User = require('../model/usercrud');
 
 router.get('/', async function (req, res, next) {
 
-  let data = await User.find();
+///////////////////////////////////////////////////////////////////////////
 
-  let delid = req.query.delid;
+  let data = await User.find();          //Find data
+
+/////////////////////////////////////////////////////////////////////////
+
+
+  let delid = req.query.delid;         //Delete data
   console.log(delid);
-
+ 
   if (delid) {
     await User.findByIdAndDelete(delid);
     return res.redirect("/");
   }
 
 
-  // let editid = req.query.editid;
-  // console.log(editid);
+////////////////////////////////////////////////////////////////////////////
 
 
+  let editid = req.query.editid             //Edit data
+  console.log(editid)
 
-  res.render('index', { title: 'Mongooes crud', data  });
+  if(editid) {
+    await User.findByIdAndUpdate(editid);
+    // return res.redirect("/");
+
+  }
+
+
+///////////////////////////////////////////////////////////////////////////
+
+  res.render('index', { title: 'Mongooes crud', data });
 
 
 });
@@ -28,19 +43,24 @@ router.get('/', async function (req, res, next) {
 
 router.post('/', async function (req, res) {
 
-  const { email, password } = req.body
- 
+////////////////////////////////////////////////////////////////////////////
+
+
+  const { email, password } = req.body             //Create data
 
   if (email && password) {
     await User.create(req.body)
     return res.redirect('/')
   }
 
+
+  ///////////////////////////////////////////////////////////////////////////
+ 
+
   // if (editid && (email || password)) {
   //   await User.findByIdAndUpdate(editid, { email, password });
   //   return res.redirect('/');
   // }
-
 
 
 })
