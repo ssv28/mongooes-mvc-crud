3a446e9bd917d8b7091f1b9bb93d0f932bcb0aa1
Your Name <you@example.com>
2024-08-06 18:46:54 +0530

    Add

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,48 @@
 let express = require('express');
 let router = express.Router();
-const User = require('../model/usercrud'); 
+const User = require('../model/usercrud');
 
-router.get('/',async function (req, res, next) {
-  let data = await User.find()
-  res.render('index', { title: 'Express' , data});
+router.get('/', async function (req, res, next) {
+
+  let data = await User.find();
+
+  let delid = req.query.delid;
+  console.log(delid);
+
+  if (delid) {
+    await User.findByIdAndDelete(delid);
+    return res.redirect("/");
+  }
+
+
+  // let editid = req.query.editid;
+  // console.log(editid);
+
+
+
+  res.render('index', { title: 'Mongooes crud', data  });
+
 
 });
 
-router.post('/',async function (req, res) {
 
-  const {email, password} = req.body
+router.post('/', async function (req, res) {
 
-  if(email && password) {
+  const { email, password } = req.body
+ 
+
+  if (email && password) {
     await User.create(req.body)
     return res.redirect('/')
   }
 
- 
+  // if (editid && (email || password)) {
+  //   await User.findByIdAndUpdate(editid, { email, password });
+  //   return res.redirect('/');
+  // }
+
+
+
 })
 
 module.exports = router;
