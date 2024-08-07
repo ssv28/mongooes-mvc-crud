6e2fbc18f88e1919a708b44ea44f1f0e4bd5929b
Your Name <you@example.com>
2024-08-07 22:05:54 +0530

    Add 7/8

--- routes/index.js
+++ routes/index.js
@@ -4,65 +4,66 @@ const User = require('../model/usercrud');
 
 router.get('/', async function (req, res, next) {
 
-  ///////////////////////////////////////////////////////////////////////////
+  //------------------------------------------------------------------------------------------
 
-  let data = await User.find();          //Find data
+  let data = await User.find();  // Find data
 
-  /////////////////////////////////////////////////////////////////////////
+  //------------------------------------------------------------------------------------------
 
-
-  let delid = req.query.delid;         //Delete data
+  let delid = req.query.delid;  // Delete data
   console.log(delid);
+
 
   if (delid) {
     await User.findByIdAndDelete(delid);
     return res.redirect("/");
+
   }
 
+  //------------------------------------------------------------------------------------------
 
-  ////////////////////////////////////////////////////////////////////////////
+  let editid = req.query.editid;  // Edit data
+  console.log(editid);
 
 
-  let editid = req.query.editid             //Edit data
-  console.log(editid)
+  let input = {};
+  console.log(input);
+
 
   if (editid) {
-    await User.findByIdAndUpdate(editid);
-    // return res.redirect("/");
+    input = await User.findById(editid);  // Find the user by ID
 
   }
 
+  //------------------------------------------------------------------------------------------
 
-  ///////////////////////////////////////////////////////////////////////////
-
-  res.render('index', { title: 'Mongooes crud', data });
-
+  res.render('index', { title: 'Mongoose CRUD', data, input, editid });
 
 });
 
+//------------------------------------------------------------------------------------------
 
 router.post('/', async function (req, res) {
 
-  ////////////////////////////////////////////////////////////////////////////
+  const { email, password, inputData } = req.body;  // Create or update data
+  console.log(req.body);
 
-
-  const { email, password } = req.body             //Create data
 
   if (email && password) {
-    await User.create(req.body)
-    return res.redirect('/')
+
+    if (inputData) {
+      await User.findByIdAndUpdate(inputData, req.body);  // Update user
+
+    }
+    else {
+      await User.create(req.body);  // Create new user
+
+    }
+
+    return res.redirect('/');
   }
 
+});
 
-  ///////////////////////////////////////////////////////////////////////////
-
-
-  // if (editid && (email || password)) {
-  //   await User.findByIdAndUpdate(editid, { email, password });
-  //   return res.redirect('/');
-  // }
-
-
-})
 
 module.exports = router;
